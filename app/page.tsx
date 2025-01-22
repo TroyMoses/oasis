d798aa88f3f01801087dc4ac9308cde044d6f3fc
TroyMoses <mugabimoses07@gmail.com>
2025-01-22 13:47:08 +0300

    logo

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Stats from "@/components/home/stats";
 import About from "@/components/home/about";
 import WhyChoose from "@/components/home/why-choose";
 import Team from "@/components/home/team";
-import Navbar from "@/components/home/navbar";
+import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
