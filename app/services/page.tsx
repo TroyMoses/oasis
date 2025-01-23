8cab41874408c5aa7cff191c2e8600627ddda7cb
TroyMoses <mugabimoses07@gmail.com>
2025-01-23 19:31:27 +0300

    services page

---
+++ app/services/page.tsx
@@ -0,0 +1,16 @@
+import Hero from "@/components/services/hero"
+import ServicesOverview from "@/components/services/services-overview"
+import DetailedServices from "@/components/services/detailed-services"
+import Team from "@/components/services/team"
+
+export default function ServicesPage() {
+  return (
+    <main>
+      <Hero />
+      <ServicesOverview />
+      <DetailedServices />
+      <Team />
+    </main>
+  )
+}
+
